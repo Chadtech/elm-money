d79ef0e0060b0437df3424f313c7372fd5658e94
Chadtech <chad.stearns@shore.com>
2018-04-02 18:06:46 +0200

    FIRST

---
+++ make-elm-money.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+const file = fs.readFileSync("./money.json", "utf-8");
+const money = Object.values(JSON.parse(file));
+
+
+fs.writeFileSync("money.elm", money.map(function(m){
+	return `
+	${m.symbol.toLowerCase()} : Currency
+	${m.symbol.toLowerCase()} =
+		{ symbol = "${m.symbol}"
+		, name = "${m.name}"
+	, namePlural = "${m.name_plural}"
+	, symbolNative = "${m.symbol_native}"
+	, decimalDigits = ${m.decimal_digits}
+	, rounding = ${m.rounding}
+	, code = "${m.code}"
+	}
+`;
+}).join());
+
+
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Elm Money
+
+Helper functions and values to make it easier to work with the world's currencies, in Elm.
